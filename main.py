ca2defaae9c84e5d7ef77addc6c49a8a99faec24
shitoujianzibu <35334213@qq.com>
2023-07-17 16:30:41 +0800

    fix: dingding

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ SOCKBOOM_USER = config("SOCKBOOM_USER")
 SOCKBOOM_PASSWD = config("SOCKBOOM_PASSWD")
 SOCKBOOM_URL = config("SOCKBOOM_URL")
 WEB_HOOK = config("WEB_HOOK")
+DING_TALK_HOOK = config("DING_TALK_HOOK")
 SHA_TZ = timezone(
 	timedelta(hours=8),
 	name='Asia/Shanghai',
@@ -41,18 +42,18 @@ def getUserInfo():
 	if userinfo.status_code == 200:
 		try:
 			# 有微信机器人
+			bs = BeautifulSoup(userinfo.text, 'html.parser')
+			ul = bs.find_all("ul", "unstyled")[0]
+			li = ul.find_all("li")
+			info = ""
+			for item in li:
+				value = item.find_all("span", "pull-right")[0]
+				label = value.previous_sibling
+				info += label.strip() + ": " + value.string + "\n"
+			utc_now = datetime.utcnow().replace(tzinfo=timezone.utc)
+			beijing_now = utc_now.astimezone(SHA_TZ)
+			print("当前时间：" + beijing_now.strftime('%Y-%m-%d %H:%M:%S') + "\n" + info)
 			if WEB_HOOK:
-				bs = BeautifulSoup(userinfo.text, 'html.parser')
-				ul = bs.find_all("ul", "unstyled")[0]
-				li = ul.find_all("li")
-				info = ""
-				for item in li:
-					value = item.find_all("span", "pull-right")[0]
-					label = value.previous_sibling
-					info += label.strip() + ": " + value.string + "\n"
-				utc_now = datetime.utcnow().replace(tzinfo=timezone.utc)
-				beijing_now = utc_now.astimezone(SHA_TZ)
-				print("当前时间：" + beijing_now.strftime('%Y-%m-%d %H:%M:%S') + "\n" + info)
 				msg_data = {
 					"msgtype": "markdown",
 					"markdown": {
@@ -63,6 +64,18 @@ def getUserInfo():
 					}
 				}
 				s.post(WEB_HOOK, json=msg_data)
+			elif DING_TALK_HOOK:
+				msg_data = {
+					"msgtype": "markdown",
+					"markdown": {
+						"title": "签到提醒",
+						"text": "当前用户: " + currentUser + "\n" 
+							+ "当前时间: " + beijing_now.strftime('%Y-%m-%d %H:%M:%S') + "\n"
+							+ "签到: <font color=\"warning\">" + checkInMsg + "</font>\n"
+							+ info
+						}
+				}
+				s.post(DING_TALK_HOOK, json=msg_data)
 		except Exception as e:
 			print(e)
 def main():
